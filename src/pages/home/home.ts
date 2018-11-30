fb04d1248b13e211cd86b42e52545cba64641f56
Karabo <Maimane23@hotmail.com>
2018-11-30 18:03:16 +0200

    fixing stuff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,11 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
-import { NavController } from 'ionic-angular';
-
+import { NavController,LoadingController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import firebase from 'firebase';
+import { signUp} from '../../app/GeoArray';
+import geoArr from '../../app/GlobalGeo';
+import { MoreInfoPage } from '../more-info/more-info';
 declare var google;
 
 @IonicPage()
@@ -17,8 +21,14 @@ export class HomePage {
   end = 'chicago, il';
   directionsService = new google.maps.DirectionsService;
   directionsDisplay = new google.maps.DirectionsRenderer;
-
-  constructor(public navCtrl: NavController) {
+  lat;
+  lng;
+  arry=[];
+  pos={};
+  address;
+  lat2;
+  lng2;
+  constructor(public loadingCtrl: LoadingController,public navCtrl: NavController, public geolocation: Geolocation) {
 
   }
 
@@ -27,27 +37,138 @@ export class HomePage {
   }
 
   initMap() {
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 17,
-      center: {lat: 41.85, lng: -87.65}
+    const loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
     });
+    loader.present();
+     this.geolocation.getCurrentPosition().then((resp) => {
 
-    this.directionsDisplay.setMap(this.map);
-  }
+      this. lat = resp.coords.latitude;
+       this. lng = resp.coords.longitude;
+       this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 18,
+        center: {lat: this.lat, lng: this.lng},
+        disableDefaultUI: true
+      });
+      let marker = new google.maps.Marker({
+        map: this.map,label:"my location",
+        position: {lat: this.lat, lng: this.lng},
+        animation: google.maps.Animation.BOUNCE,
+        icon: {
+          url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
+     
+      })
+     
+loader.dismiss();
+firebase.database().ref('userdb/').on('value', data=>{
+  let infor = data.val();
+  let arry=[]
+  let keys = Object.keys(infor);
+  console.log(keys);
 
-  calculateAndDisplayRoute() {
-    this.directionsService.route({
-      origin: this.start,
-      destination: this.end,
-      travelMode: 'DRIVING'
-    }, (response, status) => {
-      if (status === 'OK') {
-        this.directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
+ for (var i = 0; i < keys.length; i++) {
+   var k = keys[i];
+   let  obj = {
+      name: infor[k].name,
+      lat: infor[k].lat,
+      lng: infor[k].lng,
+      email:infor[k].email,
+      phone:infor[k].phone,
+      owner:infor[k].owner,
+      tel:infor[k].tel,
+      gas:infor[k].gas,
+      petrol93:infor[k].petrol93,
+      petrol95:infor[k].petrol95
+
       }
+
+    this.arry.push(obj);
+    console.log(this.arry)
+
+    this.lat2 = this.arry[i].lat;
+    this.lng2 = this.arry[i].lng;
+    console.log(this.lat + " / " + this.lng)
+      
+    // let markera: google.maps.Marker = new google.maps.Marker({
+    //   map: this.map,label:"bararararararar",
+    //   position: {lat:-26.2651693, lng:27.97542109999995}
+    // })
+
+   var marker = new google.maps.Marker({position:
+    {lat:this.arry[i].lat, lng:this.arry[i].lng}
+    ,map:this.map,
+    label:this.arry[i].name,},
+      );
+
+      var infowindow = new google.maps.InfoWindow({
+        content:obj.name
+          });
+
+
+    marker.addListener('click', () =>{
+      return new Promise(()=>{
+        geoArr.length = 0
+        var geocoder = new google.maps.Geocoder;
+        var latLng = new google.maps.LatLng(obj.lat,obj.lng);
+        infowindow.open(this.map, marker);
+        geocoder.geocode({ 'latLng': latLng }, function (results, status) {
+    
+            if (status === google.maps.GeocoderStatus.OK) {
+              var AreaName = results[0].formatted_address;
+              let areaAddress = AreaName.split(",");
+              this.streetName =areaAddress[0];
+              this.areaName = areaAddress[1];
+              this.areaLocation = areaAddress[2];
+              this.postCode = areaAddress[3];
+    
+              console.log(AreaName)
+              console.log(areaAddress);
+    
+              let obj = new signUp(this.streetName,this.areaName,this.areaLocation,this.postCode);
+              geoArr.push(obj)
+              console.log(geoArr)
+              
+          }
+          
+            console.log(this.streetName + " " + this.areaName + " " + this.postCode + " " + this.country)
+        })
+        setTimeout(()=>{
+          const loader = this.loadingCtrl.create({
+            content: "Please wait...",
+            duration: 1000
+          });
+          loader.present();
+          this.navCtrl.push('MoreInfoPage',{obj:obj})
+        },500)
+        
+      })
     });
   }
+
+})
+     })
+
+     
+
+    console.log(this.map +this.lat+this.lng );
+
+   // this.directionsDisplay.setMap(this.map);
+  }
+
+  // calculateAndDisplayRoute() {
+  //   this.directionsService.route({
+  //     origin: this.start,
+  //     destination: this.end,
+  //     travelMode: 'DRIVING'
+  //   }, (response, status) => {
+  //     if (status === 'OK') {
+  //       this.directionsDisplay.setDirections(response);
+  //     } else {
+  //       window.alert('Directions request failed due to ' + status);
+  //     }
+  //   });
+  // }
 
   nextPage(page:string){
     this.navCtrl.push(page);
--- src/pages/more-info/more-info.ts
+++ src/pages/more-info/more-info.ts
@@ -1,5 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import firebase from 'firebase';
+import { signUp} from '../../app/GeoArray';
+import geoArr from '../../app/GlobalGeo';
 declare var google;
 
 /**
@@ -14,44 +17,76 @@ declare var google;
   selector: 'page-more-info',
   templateUrl: 'more-info.html',
 })
-export class MoreInfoPage {
+export class MoreInfoPage implements OnInit{
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   start = 'chicago, il';
   end = 'chicago, il';
   directionsService = new google.maps.DirectionsService;
   directionsDisplay = new google.maps.DirectionsRenderer;
+  companyInfor;
+  businessName;
+  businessAddress;
+  businessEmail;
+  businessTel;
+  businessOwner;
+  gas;
+  p95;
+  p93;
+  diesel;
 
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController,public loadingCtrl: LoadingController,public navParams: NavParams) {
+    
   }
 
   ionViewDidLoad(){
     this.initMap();
+    this.companyInfor =this.navParams.get("obj");
+    console.log(this.companyInfor);
+    let lat = this.companyInfor.lat;
+    let lng = this.companyInfor.lng;
+    this.businessName = this.companyInfor.name;
+    this.businessEmail= this.companyInfor.email;
+    this.businessTel = this.companyInfor.tel;
+    this.businessOwner= this.companyInfor.owner;
+    this.diesel= this.companyInfor.diesel;
+    this.p93= this.companyInfor.petrol93;
+    this.p95 = this.companyInfor.petrol95;
+  
   }
 
+  ngOnInit(){
+   
+  }
   initMap() {
+    
     this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 17,
+      zoom: 15,
       center: {lat: 41.85, lng: -87.65}
     });
 
     this.directionsDisplay.setMap(this.map);
+
+ 
+
   }
 
-  calculateAndDisplayRoute() {
-    this.directionsService.route({
-      origin: this.start,
-      destination: this.end,
-      travelMode: 'DRIVING'
-    }, (response, status) => {
-      if (status === 'OK') {
-        this.directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
-  }
+
+
+  // calculateAndDisplayRoute() {
+  //   this.directionsService.route({
+  //     origin: this.start,
+  //     destination: this.end,
+  //     travelMode: 'DRIVING'
+  //   }, (response, status) => {
+  //     if (status === 'OK') {
+  //       this.directionsDisplay.setDirections(response);
+  //     } else {
+  //       window.alert('Directions request failed due to ' + status);
+  //     }
+  //   });
+  // }
 
   nextPage(page:string){
     this.navCtrl.push(page);
