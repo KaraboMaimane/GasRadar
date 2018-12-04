5c0e78888d29e3697b189b4cc87d5cfbc04b6bdc
Karabo <Maimane23@hotmail.com>
2018-12-04 10:16:25 +0200

    fixing stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import  *as firebase from 'firebase/app';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: string = 'TabsPage';
+  rootPage: string = 'LoginPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     // var config = {
