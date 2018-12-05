e5b2598a8b37f8f69cee4c3c9064dd35601040b2
Karabo <Maimane23@hotmail.com>
2018-12-05 13:33:31 +0200

    fixing some stuff

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -4,6 +4,7 @@ import { NgForm } from '@angular/forms';
 import { DatabaseProvider } from '../../providers/database/database';
 import { MediaProvider } from '../../providers/media/media';
 declare var firebase;
+declare var google;
 /**
  * Generated class for the AddPage page.
  *
@@ -29,9 +30,60 @@ export class AddPage {
 
   onAddBusiness(form: NgForm){
     // this.database.registerBusiness
-    this.database.registerBusiness(form.value).then(data=>{
-      console.log(data);
-    })
+    this.database.registerBusiness(form.value);
+    
+     let geocoder = new google.maps.Geocoder();
+      let  userid = firebase.auth().currentUser;
+   let company = form.value.name;
+   let email = form.value.email;
+   let owner = form.value.ownerName;
+   let tel = form.value.telephone;
+   let petrol93 = form.value.p93;
+   let petrol95 = form.value.p95;
+   let gas = form.value.gas;
+   let diesel = form.value.diesel;
+   let icon = form.value.business
+
+  console.log(company,email,owner,tel, petrol93,petrol95,gas, diesel,icon);
+      geocoder.geocode({'address':form.value.address},function(results, status){
+ 
+  
+  
+        if(status == google.maps.GeocoderStatus.OK){
+      
+         alert(userid);
+      
+          let lati = results[0].geometry.location.lat();
+         let longi = results[0].geometry.location.lng();
+         console.log(lati +" "+ longi);
+      
+       
+        return firebase.auth().onAuthStateChanged(data=>{
+          if(data){
+            firebase.database().ref('userdb/'+ userid).update({
+              name:company,
+              email:email,
+              owner:owner,
+              tel:tel,
+              lat:lati,
+              lng:longi,
+              petrol93:petrol93,
+              petrol95:petrol95,
+              diesel:diesel,
+              gas:gas,
+              uid:userid,
+              icon:icon
+
+  
+            })
+          }else{
+      
+            alert("login");
+          }
+      })
+        }
+          })
+ 
   }
 
   insertImage(event){
