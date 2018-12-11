f8255bb15ab8eb208f33fc1a4408e0cd8a115c28
Karabo <Maimane23@hotmail.com>
2018-12-11 14:31:09 +0200

    fixing some stuff

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, LoadingController, AlertController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { DatabaseProvider } from '../../providers/database/database';
 import { TabsPage } from '../tabs/tabs';
@@ -13,74 +13,75 @@ import { TabsPage } from '../tabs/tabs';
 
 @IonicPage()
 @Component({
- selector: 'page-login',
- templateUrl: 'login.html',
+  selector: 'page-login',
+  templateUrl: 'login.html',
 })
 export class LoginPage {
 
- action: string;
+  action: string;
 
- constructor(public loadingCtrl: LoadingController,public navCtrl: NavController, public navParams: NavParams, private database: DatabaseProvider, private toastCtrl: ToastController) {
-   this.action = 'login';
- }
+  constructor(public loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams, private database: DatabaseProvider, private loading: LoadingController, private alert: AlertController) {
+    this.action = 'login';
+  }
 
- ionViewDidLoad() {
-   console.log('ionViewDidLoad LoginPage');
- }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
 
-  register(form: NgForm){
+  register(form: NgForm) {
+
+    const loading = this.loading.create({
+      content: `Registering ${form.form.value.email}...`
+    });
+    loading.present();
     this.database.registerUser(form.form.value.name, form.form.value.email, form.form.value.password).then(
-      (data)=>{
-        const toast = this.toastCtrl.create({
-          message: 'You Have Successfully Been Registered, Please Log In',
-          duration: 3000
-        });
-        toast.present();
-        this.action = 'login'
+      (data) => {
+        loading.dismiss();
+        this.navCtrl.setRoot('MapPage');
       }
     ).catch(
-      (error)=>{
-        const toast = this.toastCtrl.create({
-          message: 'Uh Oh Something Went Wrong!',
-          duration: 3000
+      (error) => {
+        const alert = this.alert.create({
+          message: 'There Was An Error Logging You In, Please Enter Your Correct Credentials',
+          buttons: ['Ok']
         });
-        toast.present();
-        console.log(error);
+
+        alert.present();
       }
     )
   }
 
- login(form: NgForm){
-   console.log(form)
-   this.database.login(form.form.value.email,form.form.value.password).then(
-     (data)=>{
-       const toast = this.toastCtrl.create({
-         message: 'Welcome',
-         duration: 3000
-       });
-       toast.present();
-       this.navCtrl.setRoot('TabsPage');
-     }
-   ).catch(
-     (error)=>{
-       const toast = this.toastCtrl.create({
-         message: 'Uh Oh Something Went Wrong!',
-         duration: 3000
-       });
-       toast.present();
-     }
-   )
- }
- ngAfterViewInit() {
-  let tabs = document.querySelectorAll('.show-tabbar');
-  if (tabs !== null) {
-      Object.keys(tabs).map((key) => {
-          tabs[key].style.display = 'none';
-      });
-  }
-}
+  login(form: NgForm) {
+    const loading = this.loading.create({
+      content: `Registering ${form.form.value.email}...`
+    });
+    loading.present();
+    this.database.login(form.form.value.email, form.form.value.password).then(
+      (data) => {
+        loading.dismiss();
+        this.navCtrl.setRoot('TabsPage');
+      }
+    ).catch(
+      (error) => {
+        const alert = this.alert.create({
+          message: 'There Was An Error Logging You In, Please Enter Your Correct Credentials',
+          buttons: ['Ok']
+        });
 
- resetPassword(email:string){
-      this.navCtrl.setRoot('TabsPage');
+        alert.present();
+      }
+    )
+  }
+  ngAfterViewInit() {
+    let tabs = document.querySelectorAll('.show-tabbar');
+    if (tabs !== null) {
+      Object.keys(tabs).map((key) => {
+        tabs[key].style.display = 'none';
+      });
     }
   }
+
+  resetPassword(email: string) {
+    this.navCtrl.setRoot('TabsPage');
+  }
+}
